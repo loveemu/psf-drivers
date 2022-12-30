d7614ac328867de6f22b497a04d1f1c9472105be
loveemu <loveemu@gmail.com>
2022-12-31 00:31:53 +0900

    Shin Megami Tensei II: Add tagging.bat

---
+++ snsf/atlus/shin-megami-tensei-2-snsf/tagging.bat
@@ -0,0 +1,69 @@
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Title Demo" -year=1994 SHVC-ZE-2-0134.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Title" -year=1994 SHVC-ZE-2-0139.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Gym" -year=1994 SHVC-ZE-2-0116.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Virtual Battler" -year=1994 SHVC-ZE-2-0110.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Virtual Battler" -year=1994 SHVC-ZE-2-0111.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Virtual Battler" -year=1994 SHVC-ZE-2-0112.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Virtual Battler" -year=1994 SHVC-ZE-2-0147.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Shop" -year=1994 SHVC-ZE-2-0126.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Shop" -year=1994 SHVC-ZE-2-0128.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Shop" -year=1994 SHVC-ZE-2-0148.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Valhalla" -year=1994 SHVC-ZE-2-0104.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Valhalla" -year=1994 SHVC-ZE-2-0105.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Valhalla" -year=1994 SHVC-ZE-2-0114.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Valhalla" -year=1994 SHVC-ZE-2-0146.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Neutral" -year=1994 SHVC-ZE-2-0120.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Neutral" -year=1994 SHVC-ZE-2-0136.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0096.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0097.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0099.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0100.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0101.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Field" -year=1994 SHVC-ZE-2-0102.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0080.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0081.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0082.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0083.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0084.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0085.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0086.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0087.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0088.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0089.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0090.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Encounter" -year=1994 SHVC-ZE-2-0199.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Battle" -year=1994 SHVC-ZE-2-0095.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Level Up" -year=1994 SHVC-ZE-2-0094.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Recovering Remembrance" -year=1994 SHVC-ZE-2-0113.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Recovering Remembrance" -year=1994 SHVC-ZE-2-0115.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0106.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0107.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0140.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0141.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0142.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0143.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Center" -year=1994 SHVC-ZE-2-0144.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Law" -year=1994 SHVC-ZE-2-0119.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Law" -year=1994 SHVC-ZE-2-0135.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Jakyou no Yakata" -year=1994 SHVC-ZE-2-0117.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Devils Fusion" -year=1994 SHVC-ZE-2-0118.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Siren" -year=1994 SHVC-ZE-2-0098.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Underground" -year=1994 SHVC-ZE-2-0108.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Underground" -year=1994 SHVC-ZE-2-0145.minisnsf
+psfpoint -artist=Z"Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Boss Battle" -year=1994 SHVC-ZE-2-0131.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Chaos" -year=1994 SHVC-ZE-2-0121.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Chaos" -year=1994 SHVC-ZE-2-0137.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Casino" -year=1994 SHVC-ZE-2-0129.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Terminal Point" -year=1994 SHVC-ZE-2-0123.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Terminal Point" -year=1994 SHVC-ZE-2-0150.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Disco" -year=1994 SHVC-ZE-2-0130.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="2D: Makai" -year=1994 SHVC-ZE-2-0103.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Makai" -year=1994 SHVC-ZE-2-0109.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="3D: Makai" -year=1994 SHVC-ZE-2-0149.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Heroine" -year=1994 SHVC-ZE-2-0122.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Final Battle" -year=1994 SHVC-ZE-2-0132.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Final Battle" -year=1994 SHVC-ZE-2-0133.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Game Over" -year=1994 SHVC-ZE-2-0127.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Game Over" -year=1994 SHVC-ZE-2-0138.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Data Screen" -year=1994 SHVC-ZE-2-0125.minisnsf
+psfpoint -artist="Tsukasa Masuko" -copyright=Atlus -game="Shin Megami Tensei 2" -title="Applause" -year=1994 SHVC-ZE-2-0193.minisnsf
